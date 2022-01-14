d005db839a4b7291396dc6ec84492b2ecf6d9eb3
Your Name <you@example.com>
2022-01-14 11:25:27 +0300

    All

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
-var AppName = 'Mpt Lessons';
-var AppNameSelection = '%AppName%';
+var config = {
+    "AppName" : "MPT Lessons"
+};
+
+var html = document.documentElement;
+var body = document.body;
+
+const replaceHtml = function(elem, config){
+    return [elem, config]
+}
 
 document.getElementsByClassName('navbar-brand')[0].innerText
