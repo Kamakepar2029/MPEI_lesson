a3c07d1e24d232b4aa75a2fcd3759f39a697f195
Your Name <you@example.com>
2022-01-14 15:11:36 +0300

    All

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,24 @@ var config = {
 var html = document.documentElement;
 var body = document.body;
 
-const replaceHtml = function(elem, config){
-    return [elem, config];
+var lessons = document.getElementsByClassName('content-lessons')[0];
+
+const renderAllArticles = function(xhrJSONResult){
+    var template = `<div class="card-body">
+                        <h5 class="card-title">%author%</h5>
+                        <p class="card-text">%title%</p>
+                        <a href="blog.html?lesson_name=%path%" class="btn btn-primary">Open Lesson</a>
+                    </div>`;
+    for (let i in xhrJSONResult){
+        let ielem = xhrJSONResult[i];
+        let doc = document.createElement('div');
+        doc.className = 'card mb-3';
+        doc.innerHTML = template.replace('%author%', ielem["teacher"]).replace('%title%', ielem["description"]).replace("%path%", ielem["name"]);
+        lessons.innerHTML = '';
+        lessons.append(doc);
+    }
 }
 
-document.getElementsByClassName('navbar-brand')[0].innerText
+fetch('/lessons/index.json')
+  .then(response => response.json())
+  .then(response => renderAllArticles(response));
