130a3134d89f606afa5f657af34944cae1b30902
Your Name <you@example.com>
2022-01-14 16:07:21 +0300

    All

--- assets/js/article.js
+++ assets/js/article.js
@@ -7,12 +7,13 @@ var body = document.body;
 var lesson_param = params["lesson_name"];
 var article_param = params["article_param"];
 
-var lesson_container = document.getElementsByClassName('container')[0];
+var lesson_container = document.getElementsByClassName('containers')[0];
 
 const renderArticle = function(xhrJSONResult){
-    converter = new showdown.Converter(),
-    html = converter.makeHtml(xhrJSONResult);
-    lesson_container.innerHTML = html;
+    converter = new showdown.Converter();
+    lhtml = converter.makeHtml(xhrJSONResult);
+    console.log(lhtml);
+    lesson_container.innerHTML = lhtml;
 }
 
 fetch('/lessons/'+lesson_param+'/'+article_param)
