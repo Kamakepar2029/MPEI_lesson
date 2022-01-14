dc5248c0479695edf5ee0731f4ac1c6e8acce28f
Your Name <you@example.com>
2022-01-14 11:26:23 +0300

    All

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ var html = document.documentElement;
 var body = document.body;
 
 const replaceHtml = function(elem, config){
-    return [elem, config]
+    return [elem, config];
 }
 
 document.getElementsByClassName('navbar-brand')[0].innerText
