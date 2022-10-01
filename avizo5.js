388fcfdb31886220a542d9c721ffe5748bbbd7a1
Cajemam Carente <106678041+cajemam@users.noreply.github.com>
2022-10-01 14:20:40 +0300

    Update avizo5.js

--- avizo5.js
+++ avizo5.js
@@ -1,4 +1,4 @@
-let extension = document.createElement('div');
+var extension = document.createElement('div');
 extension.setAttribute('style', "position: fixed;left: 0px;bottom: 0px;z-index: 1000000000;background: #fff;padding: 10px 20px;border-radius: 5px;font-family: 'Roboto', sans-serif;");
 extension.innerHTML = '<h1 style="text-align: center;padding-bottom: 7px;font-size: 20px;"> Aviso Autosurf</h1><div class="extension_enabled">Extension: <green style="border: 1px solid #01a101;background: #00c700;color: white;border-radius: 10px;padding: 4px 18px;">enabled</green></div>';
 document.body.append(extension);
@@ -28,8 +28,8 @@ function checkYoutube(){
     if (block.getAttribute('style') == youtubes){
         console.log('Everything ok');
     }else{
+         document.querySelector('.extension_enabled').innerHTML = "Redirecting...";
          avizoJavascriptHandler.youtubeReady();
-         document.write('Youtube ready.');
     }
 }
 
